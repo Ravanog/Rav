79bac4c161c58a8259922138f001e98f524efc03
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-24 19:38:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 25830285
 API_HASH     = "0cbcabbf1b9c56a4dfc4b8950d584a2a"
-BOT_TOKEN    = "6537674730:AAHsRA9ViM0xYZPDWf-zbWljxRwKK6zIEB4"
+BOT_TOKEN    = "6576609270:AAGLAbzgaVGfrr5z2xWXA-gC_fngjA-Krkc"
 SESSION      = "BQGKI40AVVcKOvSmI0uF_H6p5Z4d25thXtODsoh50DXEO4_mwUd4E96wKy-YECOESchasAiNfwMRj1EurRD92nfqBfUK4V8XlmXq3sE1EG5rfXuSGSl-M5u0sZ_a4NEyF4Oj4_498aEt9rsQunF9SPQN8HxWZseT0r6nu8CK03fi7mreaP_IPuB-pm_yO_Bc4qDkDGL6mgBIM-27y_BQ1EQSW3mpEjJQyWwqBxKbxljblxCmsLapp2-L_OS2Mb7_0oQUmfOF3v3qMg8b4pSPEPxz6ni8zs2bJPLxlw6SyWEy53Jtkcps4pBRPdPr6VavkqOBY63ACy4YyrtgiE3gbeHXs6mX7wAAAAGFr9qdAA"
 DATABASE_URI = "mongodb+srv://Searchbot:Mongodb.bot@cluster0.5xixyjh.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
