21f3e0fc0afbacfdc2f37a0cc8b1d56450873222
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-25 16:31:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,4 +5,4 @@ SESSION      = "BQDymN4AV-0I-JIzscjHeCtn7ZF69AFR5oHF9QXrcCBUmKse1GIx-MhTvP7rM1n1
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
 ADMIN        = 6651109872
-AUTO_DELETE_TIME  = "100"
+
