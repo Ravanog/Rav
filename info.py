1a5e8dbfb2aba212630d8683e696ca41e56a3838
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-28 18:22:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
-API_ID       = 25830285
-API_HASH     = "0cbcabbf1b9c56a4dfc4b8950d584a2a"
+API_ID       = 24673802
+API_HASH     = "c303a76d9d0fdb5dfb0a8fc4f44637ea"
 BOT_TOKEN    = "6576609270:AAGLAbzgaVGfrr5z2xWXA-gC_fngjA-Krkc"
 SESSION      = "BQF4fgoARpHpH3VcK0iZAhDeKDiQ7Te3-GKhy-OHoCQ9_PCcbGhpe8aoPcBRbAhxCHFwZZP_j-SLuinZpl2FfHa6uBJRlg3n_fPyfpQVPkcSRieo1JKqwCGIt5d0S22A0kNDLzCplNrYcWsJA2LMq7DqMr6jfQdwfxRjNKVwSoJ0vQd4w5nICPmq_1sohf4QPgjWzuzK8fIK_dxPIQvfbHpTDzg-S3d4EI_Eej-4zgh5uGOpfp8UA1YMyST2M20sGQE96gVrSo4Dkqw9jm7GRQWWzTZUlOl6W5qtT7zwcYhryXPKQb-3Aglgk9Y1HkWPSbwR7R6ofIruTTI0b5CWkwbKRqwTJwAAAAF1aAloAA"
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
