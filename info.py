e7101a52236fea8b5c49f7874ab4a18d50d31bbf
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-28 18:17:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 25830285
 API_HASH     = "0cbcabbf1b9c56a4dfc4b8950d584a2a"
 BOT_TOKEN    = "6576609270:AAGLAbzgaVGfrr5z2xWXA-gC_fngjA-Krkc"
-SESSION      = "BQDymN4AV-0I-JIzscjHeCtn7ZF69AFR5oHF9QXrcCBUmKse1GIx-MhTvP7rM1n1G6aWpNqoRaJHMX_ozBhg4FvSJr2bStsrcmLNl3LT6clrI5vvxfRo0FRMxBso2z0JfBh7wNcUyqiyyVRlpzo7Gbxpvm2dDlHocHlrFp0fKbSKbf4kV_IkAegNP3whLauI-0oAz5ZZ9EU3oti8QIXUQZBMPoCACu1tmi8YjK8sm7TDjE7TAkFPyhHE_DGuVWnBkYb2Rhoa002u1T87eh5xDiuzExbvWOTxJS2qiks3PRxBbcnqRCF6Pyy2B_j4zxZfHO6L0Ro2-HAoeNgCDAfhH2PhQg4qawAAAAE96hcYAA"
+SESSION      = "BQF4fgoARpHpH3VcK0iZAhDeKDiQ7Te3-GKhy-OHoCQ9_PCcbGhpe8aoPcBRbAhxCHFwZZP_j-SLuinZpl2FfHa6uBJRlg3n_fPyfpQVPkcSRieo1JKqwCGIt5d0S22A0kNDLzCplNrYcWsJA2LMq7DqMr6jfQdwfxRjNKVwSoJ0vQd4w5nICPmq_1sohf4QPgjWzuzK8fIK_dxPIQvfbHpTDzg-S3d4EI_Eej-4zgh5uGOpfp8UA1YMyST2M20sGQE96gVrSo4Dkqw9jm7GRQWWzTZUlOl6W5qtT7zwcYhryXPKQb-3Aglgk9Y1HkWPSbwR7R6ofIruTTI0b5CWkwbKRqwTJwAAAAF1aAloAA"
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
 ADMIN        = 6651109872
