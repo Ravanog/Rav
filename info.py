4893d181666d1c6bb1c52d347629ae515ce1d99e
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-10-14 16:11:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 28280795
 API_HASH     = "3f02c2df6069de8c1a2abf623da0a4b8"
-BOT_TOKEN    = "6540749009:AAEq2GGDmb3_ONCFpjMclNS__LoDoEzy1rc"
+BOT_TOKEN    = "6576609270:AAGLAbzgaVGfrr5z2xWXA-gC_fngjA-Krkc"
 SESSION      = "BQGvh9sAHJ-MwRwFRsWu-d1mz57jXW00XvCKoqBTaEd3KlpFivh62_Rl7K8dZsyhh7osN71k2O27YU-_tQpsTkf0TlNqztChfSyMQDEctvw1RCfMVfv9o0bSv4gIEm5n10LEM1OvqoMYCnbfMeMMkl0H_by1BVedg-QO_nglD_DqgcIbpW-vIm_uBIsvSpdePSjJtpNt20c9csvUcg2JO9PX35Iek_6hassp3QGJsjhFBRsRHDRKJBhz7DXSiROnDsQWR9j5qNCIPNs0S6jR5ajiUbUm9hQcUk7JtGypjRhHruD6JYaWIeenbsyIoUGAauGjXfDEOWf5JVUCh36VhKrEyIRuuwAAAAGMb-HwAA"
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
@@ -10,4 +10,4 @@ APPROVETEXT  = "Hᴇʟʟᴏ {mention}\ɴWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏ�
 APPROVE     = "on"
 OWNER_USERNAME = "ROYAL_GAGAN_DEEP_SINGH_MEHRA"
 UPI_PIC = "https://telegra.ph/file/e28b3c353c24706fb67d8.jpg"
-BOT_USERNAME = "Post_finder_pro_bot"
+BOT_USERNAME = "Channel_Post_link_search_bot"
