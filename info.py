9bdfa74de1a836fe8c18c6e6a4d8ecf46a688e55
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-10-14 15:02:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 24673802
 API_HASH     = "c303a76d9d0fdb5dfb0a8fc4f44637ea"
-BOT_TOKEN    = "6576609270:AAGLAbzgaVGfrr5z2xWXA-gC_fngjA-Krkc"
+BOT_TOKEN    = "6540749009:AAEq2GGDmb3_ONCFpjMclNS__LoDoEzy1rc"
 SESSION      = "BQF4fgoAuh0iiSEEPTYpj9I-nAHkbyy5XBfOl85ESvsffgRQ5lCUOLtllIcqanRGe9jFBeVj9dSmGtNDZOzoLE6YaeeEU64-FrZdmEpU0uGgAzBaUcUmVf9qhiraO0XKVEY0vztWG4eBazjgZUlXwo_qdVZDtasexThGPKxIOa_P71ly17RIlGJRxWs3hoUBb-pHaEukkjE2z3RLco-e5wl0boNufKUa4ZHjnDM9V5LZiRfSPQe59s6_jZ0CFDpQqT_GVsMrYg-HUHGVnYfw9S2_r4BvmnWYeiVBEliY1PmWkCGboI8A4Ooq1GUpUiGpodNS1LNISa-zerN0hPlt-vrmJ_HW-wAAAAF1aAloAA"
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
