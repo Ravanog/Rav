3a1a76a299c7ecb477b0bc468c4d196e2740b5f3
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-10-14 14:43:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,4 +5,9 @@ SESSION      = "BQF4fgoAuh0iiSEEPTYpj9I-nAHkbyy5XBfOl85ESvsffgRQ5lCUOLtllIcqanRG
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
 ADMIN        = 6651109872
-
+CHANNEL      = "-1001953069847"
+APPROVETEXT  = "Hᴇʟʟᴏ {mention}\ɴWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏᴜʀ ʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴘᴘʀᴏᴠᴇᴅ"
+APPROVE     = "on"
+OWNER_USERNAME = "ROYAL_GAGAN_DEEP_SINGH_MEHRA"
+UPI_PIC = "https://telegra.ph/file/e28b3c353c24706fb67d8.jpg"
+BOT_USERNAME = "Post_finder_pro_bot"
