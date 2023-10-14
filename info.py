1d23360078c227b93de0753e1136268c79d3bf84
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-10-14 15:10:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,7 +5,7 @@ SESSION      = "BQF4fgoAuh0iiSEEPTYpj9I-nAHkbyy5XBfOl85ESvsffgRQ5lCUOLtllIcqanRG
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
 ADMIN        = 6651109872
-CHANNEL      = "-1001953069847"
+CHANNEL      = "-1001929711696"
 APPROVETEXT  = "Hᴇʟʟᴏ {mention}\ɴWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏᴜʀ ʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴘᴘʀᴏᴠᴇᴅ"
 APPROVE     = "on"
 OWNER_USERNAME = "ROYAL_GAGAN_DEEP_SINGH_MEHRA"
