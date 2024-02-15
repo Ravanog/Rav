5c413eddd2884f7ed0e94d873409fbdd142f016d
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-02-15 22:08:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,7 +5,7 @@ SESSION      = "BQF4fgoAiqycYghFJALZSeCEzXQbTsLLNNH3Sczo2hHnQHN8BN3fMqeSYPFGvJ-2
 DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
 ADMIN        = 6651109872
-CHANNEL      = "-1001929711696"
+CHANNEL      = "-1001876488513"
 APPROVETEXT  = "Hᴇʟʟᴏ {mention}\nWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏᴜʀ ʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴘᴘʀᴏᴠᴇᴅ"
 APPROVE     = "on"
 OWNER_USERNAME = "ROYAL_GAGAN_DEEP_SINGH_MEHRA"
