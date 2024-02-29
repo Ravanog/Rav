b2585ac3bf89368f0beb8b6a6c1d41be0c45c579
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-02-29 17:41:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 28280795
 API_HASH     = "3f02c2df6069de8c1a2abf623da0a4b8"
 BOT_TOKEN    = "6537674730:AAHsRA9ViM0xYZPDWf-zbWljxRwKK6zIEB4"
 SESSION      = "BQGKI40AMJR42mmtVmAlQJ1IkuPDH2JGTS-jv61T_42jrUMSKkX3wCJDqzbnF2pu8XsSDI27I9y2x5OE0LDYIWqGu5N-STmp92A0h6RW969HwjynuMKQCPExXyV5k0HEjpvGJxGi80QQhxCgRPzRBe9vUE-vB2BAi1ZSsYOSVHTHjfueh2YM95KQSQm_UYx7oG2Jeb6vWpro-E9tjtzeIRXCWSLliKTTczpFtPQPCBjU5GOL_bjzJVdigHrBDwBwd4AUaELJ7ZXX-ZzNKYeOA4RlcI3we3zj_sOQo2MVrNxkMc1qK7TiIMQZFiXlzsssNyEQ-G4qF90q6QEF6MGeUiR6fmnGSQAAAAGbcyXeAA"
-DATABASE_URI = "mongodb+srv://Mdisk-Search-Bot:mdisksearchbot@cluster0.bbnr7ke.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Lk:0@cluster0.y64v0be.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1001961336094
 ADMIN        = 6651109872
 CHANNEL      = "-1001961336094"
