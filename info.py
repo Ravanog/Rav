e1248cd3be5197a97d13a82ec08c954867cd1734
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-04-21 20:57:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 28280795
-API_HASH     = "3f02c2df6069de8c1a2abf623da0a4b8"
-BOT_TOKEN    = "6537674730:AAHsRA9ViM0xYZPDWf-zbWljxRwKK6zIEB4"
-SESSION      = "BQGKI40AW_FC5YizgXjxJSAa-JGX6zDwDnzd_rOVvN0gCdpBo-PAlWQplpUqPzeaE6mDhvv10zYPSOFk5_h8VFM2h5gHXXbItYm-uK8P0Gcu-Tr_6AHmPKzM9r5UAsCU5RqT1vc08Ceb1Zm9HPzHsQ88p7gxKF8b9cudpVR4qJErjH59CprBb9TZQKf6M0u06suHSw8GRihvBr3dOtEQUxOiPAyISBKEf0huiaePfCJMYhrui9uS9bTcFKRV7U7eNGo_kmNdjsBS4bhNsHNADtL1uxIw33E_ubtReIxTQz6Sm38RHVPaUadKBr3hiEYiVql_6WScwl0k93Pue0FamidJWH7IvAAAAAGbcyXeAA"
-DATABASE_URI = "mongodb+srv://Lk:0@cluster0.y64v0be.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+API_ID       = 
+API_HASH     = ""
+BOT_TOKEN    = ""
+SESSION      = ""
+DATABASE_URI = ""
 LOG_CHANNEL  = -1001961336094
 ADMIN        = 6651109872
 CHANNEL      = "-1001961336094"
