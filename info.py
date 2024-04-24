10e3d382e5df97e78a9fd55ddaccb55a2ce07411
Ravanog <149065101+Ravanog@users.noreply.github.com>
2024-04-24 11:30:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 22971260
 API_HASH     = "39b93b45f525642b9c732f43185e4ab0"
-BOT_TOKEN    = "6618768721:AAEiyrYIj7f9x9qbI55vKmHBgEDLnFmdwjY"
+BOT_TOKEN    = "6774405476:AAHB-RMY8GDJHhLDAHm68y4H3VhYVgmn3HA"
 SESSION      = "BQF4fgoAv0yrMlgiSnpJgm_jTBW8Tx8k9I5oNNJ8q-Ro9LTKVFghdKqoQayZ_aCNr2bjfqH-3KTJfY2wpV-pgqMbgBxG0_zAQLqPGvvV2QVuySnl5r-7PAbdrHs687xrKkfXHDjNEzy6fgy8_PkaOQiY-SjE9TAkGSis3zTt8ohA3Y66kr6MqCov-FFZQ4BDydtyfZW5IufdYKNPfff-BqdxszO2NSSQar7PUtyacEaU62OmI2dtixg88fDy1w-r_J_Z924ot8SPyLr3RtBNaADCSX6VhPLtEo1Bm4X9r8bPgvr9VQ7EGLEsMtKKqhTiid2OXYYkIJromBkDABwXdhkXzT2f_gAAAAF1aAloAA"
 DATABASE_URI = "mongodb+srv://Harsh:Harsh@cluster0.n18lz2v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1002056822145
@@ -9,5 +9,5 @@ CHANNEL      = "-1002039419270"
 APPROVETEXT  = "Hᴇʟʟᴏ {mention}\nWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏᴜʀ ʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴘᴘʀᴏᴠᴇᴅ"
 APPROVE     = "on"
 OWNER_USERNAME = "KUSHALHK"
-UPI_PIC = "https://telegra.ph/file/e28b3c353c24706fb67d8.jpg"
-BOT_USERNAME = "Jukukush_bot"
+UPI_PIC = "https://graph.org/file/c00f6eff2929a1b000ee8.jpg"
+BOT_USERNAME = "Post_Search_HK_bot"
