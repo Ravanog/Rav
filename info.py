9c1ff632aa651ff97eb9131a2df4fe51c45d537d
Ravanog <149065101+Ravanog@users.noreply.github.com>
2024-04-25 07:56:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ API_HASH     = "39b93b45f525642b9c732f43185e4ab0"
 BOT_TOKEN    = "6774405476:AAHB-RMY8GDJHhLDAHm68y4H3VhYVgmn3HA"
 SESSION      = "BQDvISsAFJNQ7K-5Wr4Z9jcIzOVvhtemNsaaxlgi8K1NvEMt3QR8ULZ2abtl5ljATZ87mF9TFrTvUAq9y_vGio98zz0rdzmi70aSPVi5oO77N7FNSfBr3j97_uUGQcP4iQO-FB9DBa7HGdLT-q0HyiZqWsM4JRk1FGsNbFcdxBxzQNFawPvxBG1kVN0hL61OazthF6YTp2E5BwQhF9S_psF1Ju_Ouz9HBUkfeI235sXTKf-uYs_KWWoolfxAQvJTK_larMDCtbNu55Jo1pVnUFDCfx75V8dUSg1Pnda909otpvVfVWSGeqpXFX879S1CED_zFsKkSOh9aqUjBEJp1ppFxTbEWAAAAAFgWo6YAA"
 DATABASE_URI = "mongodb+srv://Ps:ps@cluster0.ptgbmd7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1002039419270
+LOG_CHANNEL  = -1002056822145
 ADMIN        = 5911514776
 CHANNEL      = "-1002039419270"
 APPROVETEXT  = "Hᴇʟʟᴏ {mention}\nWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏᴜʀ ʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴘᴘʀᴏᴠᴇᴅ"
